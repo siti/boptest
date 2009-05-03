a98a8e014fef51a9249528de8e9b7db21e3d8fa1
Stephen Cook <sitiveni@gmail.com>
2009-05-03 23:14:04 +1200

    fix getdll... the compiler was optimizing the value to be static previously :(

--- random/getdll.c
+++ random/getdll.c
@@ -1,8 +1,20 @@
-#include <stdlib.h>
+/* getdll.c - Get the address of a function in a shared library and print it
+ *
+ * Copyright (c)2003 by Peter Busser <peter@adamantix.org>
+ * This file has been released under the GNU Public Licence version 2 or later
+ */
+
+#define _GNU_SOURCE
 #include <stdio.h>
+#include <stdlib.h>
+#include <dlfcn.h>
 
 int main(int argc, char **argv)
 {
-	printf("%p\n", system);
+	void *handle = dlopen(RTLD_DEFAULT, RTLD_LAZY);
+	if (!handle)
+		return 1;
+	printf("%p\n", dlsym(handle, "system"));
+	dlclose(handle);
 	return 0;
 }
